35639ea334c766229b47dd7bb375d46fa3a86615
Biman Gunarathne <bimangunarathne@outlook.com>
2023-01-06 20:34:54 +0530

    responsicve the contact us ui and change the icons

--- mainuis/contact_us.php
+++ mainuis/contact_us.php
@@ -31,7 +31,7 @@
     <div class="small-div-container">
         <div class="small_div_1">
             <div class="lottie">
-                <lottie-player class="lottie-all-div" src="https://assets1.lottiefiles.com/packages/lf20_Yd38lY.json" background="transparent" speed="1" hover autoplay></lottie-player>
+                <lottie-player class="lottie-all-div" src="https://assets10.lottiefiles.com/packages/lf20_metr6yqs.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
             <p class="big_font">Address</p>
             <p class="p_font">No.84/18A, Koongahawatta,<br>Kotikawatta Road, Gothatuwa</p>
@@ -39,7 +39,7 @@
         <div class="line"></div>
         <div class="small_div_2">
             <div class="lottie">
-                <lottie-player class="lottie-all-div" src="https://assets10.lottiefiles.com/packages/lf20_t05opzok.json" background="transparent" speed="1" hover autoplay></lottie-player>
+                <lottie-player class="lottie-all-div" src="https://assets6.lottiefiles.com/packages/lf20_i8k8j9Alxm.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
             <p class="big_font">Email</p>
             <p class="p_font">contact@opdechanneling.lk</p>
@@ -47,7 +47,7 @@
         <div class="line"></div>
         <div class="small_div_3">
             <div class="lottie">
-                <lottie-player class="lottie-all-div" src="https://assets6.lottiefiles.com/private_files/lf30_ctwz0ajo.json" background="transparent" speed="1" hover autoplay></lottie-player>
+                <lottie-player class="lottie-all-div" src="https://assets2.lottiefiles.com/packages/lf20_vcxfcjv2.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
             <p class="big_font">Telephone</p>
             <p class="p_font">+94 112233445 - Hed Office</p>
