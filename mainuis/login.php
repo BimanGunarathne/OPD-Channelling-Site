03b808758e4441edc9f1d3035145f18a125eafa3
Biman Gunarathne <bimangunarathne@outlook.com>
2023-01-08 01:53:19 +0530

    Redesighne login page

--- mainuis/login.php
+++ mainuis/login.php
@@ -16,13 +16,14 @@
             <div class="showcase-content">
                 <div class="description">
                     <h1>Log In</h1>
-                    <!-- <p class="p_font">You can send a message and feedbacks to us by filling the below fields.</p> -->
+                    <p class="p1">Welcome! Please fill the details</p>
+                    <a class="buttons btn-google" href=""><img class="google" src="../images/icons8-google-48.png" alt="google">Signup with Google</a>
+                    <p class="por"><span> or</span></p>
                     <input class="inputs" type="text" placeholder="Username or Email" value="">
-                    <input class="inputs" type="password" placeholder="Password" value="">
+                    <input class="i1 inputs" type="password" placeholder="Password" value="">
+                    <a class="fp" href="">Forgot Password?</a>
                     <a class="buttons btn-prymery" href="">Log In</a>
-                    <a class="buttons btn-secondory" href="">Sign Up</a>
-                    <p>or</p>
-                    <a class="buttons btn-google" href="">Signup with google</a>
+                    <p class="p2">Dont have an account? <a class="signup" href="">Signup</a></p>
                 </div>
                 <lottie-player class="lottie-player" src="https://assets9.lottiefiles.com/packages/lf20_hzgq1iov.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
