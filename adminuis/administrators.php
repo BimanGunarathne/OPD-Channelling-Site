fb6b4afcf131674ea25e0be6de70c9b7428fdf7f
Biman Gunarathne <bimangunarathne@outlook.com>
2023-01-21 21:36:35 +0530

    Start to create administration

---
+++ adminuis/administrators.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrator</title>
+</head>
+
+<body>
+    <?php include '../includes/navbar2.php' ?>
+    <header>
+        <?php include '../includes/admin_main.php' ?>
+    </header>
+    <?php include '../includes/footer2.php' ?>
+</body>
+
+</html>
---
+++ includes/admin_main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/administrator.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <header class="showcase">
+        <div class="container1">
+            <div class="showcase-content">
+                <div class="description">
+                    <h1>Welcome to OPD Channelling Administrator</h1>
+                    <p class="description2">
+                        Thsi is an Administrator section in the website. Admins can add administrators from the Admin Manager page, as well as delete admins. Admins can add, update and delete hospitals from the Manage hospitals page.
+                        Admins can manage doctors, patients and appointments as well. And also can view their admin profile from the profile page and update the details as well their password. So please be carefull using the admin section.
+                    </p>
+                    <a class="btn-primary" href="../php UI/contact_us.php">Admin Profile</a>
+                </div>
+                <lottie-player class="lottie-player" src="https://assets4.lottiefiles.com/packages/lf20_SPA6bgo7nO.json" background="transparent" speed="1" loop autoplay></lottie-player>
+            </div>
+        </div>
+    </header>
+</body>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@1.5.7/dist/lottie-player.js"></script>
+<script src="/node_modules/@lottiefiles/lottie-player/dist/lottie-player.js"></script>
+<script src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/tgs-player.js"></script>
+<script src="/node_modules/@lottiefiles/lottie-player/dist/tgs-player.js"></script>
+
+</html>
