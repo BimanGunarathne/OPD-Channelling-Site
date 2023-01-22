45e14254987a91827258315cf6ea8712a92fb918
Biman Gunarathne <bimangunarathne@outlook.com>
2023-01-22 10:31:57 +0530

    Create administration page

---
+++ adminuis/administrator.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/administrator.css">
+    <title>Administrator</title>
+</head>
+
+<body>
+    <?php include '../includes/navbar2.php' ?>
+    <div class="admin_content">
+        <div class="admin_name">
+            <div class="admin_username">
+                <h2>Administration</h2>
+            </div>
+        </div>
+        <div class="admin_section">
+            <div class="admin_details">
+                <div class="space">
+                    <h3>First Name:</h3>
+                    <input type="text" name="firstname" placeholder="First Name">
+                </div>
+                <div class="space">
+                    <h3>Last Name:</h3>
+                    <input type="text" name="lastname" placeholder="Last Name">
+                </div>
+                <div class="space">
+                    <h3>E-Mail:</h3>
+                    <input type="email" name="email" placeholder="E-Mail">
+                </div>
+                <div class="space">
+                    <h3>N.I.C. Number:</h3>
+                    <input type="text" name="nic" placeholder="N.I.C. Number">
+                </div>
+                <div class="space">
+                    <h3>Contact Number:</h3>
+                    <input type="tel" name="tel" placeholder="Contact Number">
+                </div>
+                <div class="space">
+                    <h3>Username:</h3>
+                    <input type="text" name="username" placeholder="Username">
+                </div>
+                <div class="space">
+                    <h3>Password:</h3>
+                    <input type="password" name="password" placeholder="Passowrd">
+                </div>
+                <div class="space">
+                    <h3>Retype Password:</h3>
+                    <input type="password" name="retype-password" placeholder="Retype Passowrd">
+                </div>
+                <div class="space buttons">
+                    <button class="button_class">Save</button>
+                    <button class="button_class2">Change</button>
+                    <button class="button_class2 red_buttion">Delete</button>
+                </div>
+            </div>
+        </div>
+        <div class="admin_table">
+            <div class="admin_h1">
+                <h2>Administartors</h2>
+            </div>
+            <Table>
+                <tr>
+                    <td class="bg">Admin ID</td>
+                    <td class="bg">First Name</td>
+                    <td class="bg">Last Name</td>
+                    <td class="bg">NIC</td>
+                    <td class="bg">Contact</td>
+                    <td class="bg">Email</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Biman</td>
+                    <td>Gunarathne</td>
+                    <td>200016403180</td>
+                    <td>0771144033</td>
+                    <td>bimangunarathne@gmail.com</td>
+                </tr>
+            </Table>
+        </div>
+    </div>
+    <?php include '../includes/footer2.php' ?>
+</body>
+
+</html>
--- includes/admin_main.php
+++ includes/admin_main.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/administrator.css">
+    <link rel="stylesheet" href="../css/admin_home.css">
     <title>Home</title>
 </head>
 
@@ -19,7 +19,7 @@
                         Thsi is an Administrator section in the website. Admins can add administrators from the Admin Manager page, as well as delete admins. Admins can add, update and delete hospitals from the Manage hospitals page.
                         Admins can manage doctors, patients and appointments as well. And also can view their admin profile from the profile page and update the details as well their password. So please be carefull using the admin section.
                     </p>
-                    <a class="btn-primary" href="../php UI/contact_us.php">Admin Profile</a>
+                    <a class="btn-primary" href="../adminuis/administrator.php">Admin Profile</a>
                 </div>
                 <lottie-player class="lottie-player" src="https://assets4.lottiefiles.com/packages/lf20_SPA6bgo7nO.json" background="transparent" speed="1" loop autoplay></lottie-player>
             </div>
